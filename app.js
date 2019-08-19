f0d3ecb9a624832b14c5a77a8164d2ad21571da3
Joshua Fitzgerald <fitzgerald913@gmail.com>
2019-08-19 11:36:37 -0500

    added post routes and column imageUrl to products

--- app.js
+++ app.js
@@ -1,16 +1,62 @@
-var express = require('express');
-var path = require('path');
+const express = require("express");
+const path = require("path");
+const models = require("./models");
 
-
-var app = express();
-
+const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.post("/user", async (req, res, next) => {
+  const username = req.body.name;
+  const password = req.body.password;
+  const user = models.User.build({
+    username: username,
+    password: password
+  });
+  try {
+    const response = await user.save();
+    res.json(response);
+  } catch (error) {
+    res.json(error);
+  }
+});
 
+app.post("/vendor", async (req, res, next) => {
+  const name = req.body.name;
+  const vendor = models.Vendor.build({
+    name
+  });
+  try {
+    const response = await vendor.save();
+    res.json(response);
+  } catch (error) {
+    res.json(error);
+  }
+});
 
-
+app.post("/product", async (req, res, next) => {
+  const name = req.body.name;
+  const price = parseInt(req.body.price);
+  const ageRestricted = req.body.ageRestricted === "true" ? true : false;
+  const vendorId = parseInt(req.body.vendorId);
+  const type = req.body.type;
+  const imageUrl = req.body.imageUrl;
+  const product = models.Product.build({
+    name,
+    price,
+    ageRestricted,
+    vendorId,
+    type,
+    imageUrl
+  });
+  try {
+    const response = await product.save();
+    res.json(response);
+  } catch (error) {
+    res.json(error);
+  }
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -21,11 +67,11 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
