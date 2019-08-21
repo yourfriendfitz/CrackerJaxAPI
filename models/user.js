8f6c2b9b31c35fff291834c854e4c3e816ad429b
Joshua Fitzgerald <fitzgerald913@gmail.com>
2019-08-21 12:15:15 -0500

    removed password from users table

--- models/user.js
+++ models/user.js
@@ -1,9 +1,12 @@
-'use strict';
+"use strict";
 module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define('User', {
-    username: DataTypes.STRING,
-    password: DataTypes.STRING
-  }, {});
+  const User = sequelize.define(
+    "User",
+    {
+      username: DataTypes.STRING
+    },
+    {}
+  );
   User.associate = function(models) {
     // associations can be defined here
     User.hasMany(models.Order, { as: "orders", foreignKey: "uid" });
